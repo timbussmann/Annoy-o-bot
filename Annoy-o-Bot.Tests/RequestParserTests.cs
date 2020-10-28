921fe70d07042a7181df15806ce0018391e758e6
Tim Bussmann <timbussmann@users.noreply.github.com>
2020-10-28 23:57:50 +0100

    map commit message

--- Annoy-o-Bot.Tests/RequestParserTests.cs
+++ Annoy-o-Bot.Tests/RequestParserTests.cs
@@ -21,6 +21,7 @@ namespace Annoy_o_Bot.Tests
             Assert.Equal("ba7c6f17f5beaafc603eca52b864356848865fec", commit.Id);
             var addedFile = Assert.Single(commit.Added);
             Assert.Equal(".reminder/testReminder.json", addedFile);
+            Assert.Equal("Create trigger4.json", commit.Message);
 
             Assert.Equal("timbussmann", result.Pusher.Name);
             
@@ -36,18 +37,22 @@ namespace Annoy_o_Bot.Tests
             Assert.Equal(".reminder/newFile.json", Assert.Single(result.Commits[0].Added));
             Assert.Empty(result.Commits[0].Modified);
             Assert.Empty(result.Commits[0].Removed);
+            Assert.Equal("Create newFile.json", result.Commits[0].Message);
 
             Assert.Equal(".reminder/newFile.json", Assert.Single(result.Commits[1].Modified));
             Assert.Empty(result.Commits[1].Added);
             Assert.Empty(result.Commits[1].Removed);
+            Assert.Equal("Update newFile.json", result.Commits[1].Message);
 
             Assert.Equal(".reminder/newFile.json", Assert.Single(result.Commits[2].Removed));
             Assert.Empty(result.Commits[2].Added);
             Assert.Empty(result.Commits[2].Modified);
+            Assert.Equal("Delete newFile.json", result.Commits[2].Message);
 
             Assert.Empty(result.Commits[3].Added);
             Assert.Empty(result.Commits[3].Modified);
             Assert.Empty(result.Commits[3].Removed);
+            Assert.Equal("Merge pull request #15 from timbussmann/file-ops-history\n\nCreate newFile.json", result.Commits[3].Message);
 
             Assert.Equal("cb1ec97f51657c2718ab4e0b1d0bf2656aeb3127", result.HeadCommit.Id);
         }
