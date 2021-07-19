9f66499e0c1e2f6c72a580ed58052ead4c50d940
Tim Bussmann <timbussmann@users.noreply.github.com>
2021-07-19 13:03:03 +0200

    new commit parsing logic

--- Annoy-o-Bot/CommitParser.cs
+++ Annoy-o-Bot/CommitParser.cs
@@ -3,8 +3,55 @@ using System.Linq;
 
 namespace Annoy_o_Bot
 {
+    public class FileChanges
+    {
+        public HashSet<string> New { get; set; } = new HashSet<string>();
+        public HashSet<string> Updated { get; set; } = new HashSet<string>();
+        public HashSet<string> Deleted { get; set; } = new HashSet<string>();
+    }
+
     public class CommitParser
     {
+        public static FileChanges GetChanges(CallbackModel.CommitModel[] commits)
+        {
+            var changes = new FileChanges();
+
+            foreach (var commit in commits)
+            {
+                foreach (var added in commit.Added)
+                {
+                    if (changes.Deleted.Contains(added))
+                    {
+                        changes.Deleted.Remove(added);
+                        changes.Updated.Add(added);
+                    }
+                    else
+                    {
+                        changes.Updated.Remove(added);
+                        changes.New.Add(added);
+                    }
+                }
+
+                foreach (var modified in commit.Modified)
+                {
+                    changes.Deleted.Remove(modified);
+                    if (!changes.New.Contains(modified))
+                    {
+                        changes.Updated.Add(modified);
+                    }
+                }
+
+                foreach (var removed in commit.Removed)
+                {
+                    changes.New.Remove(removed);
+                    changes.Updated.Remove(removed);
+                    changes.Deleted.Add(removed);
+                }
+            }
+
+            return changes;
+        }
+
         public static string[] GetReminders(CallbackModel.CommitModel[] commits)
         {
             return commits.Aggregate(new HashSet<string>(), (added, commit) =>
