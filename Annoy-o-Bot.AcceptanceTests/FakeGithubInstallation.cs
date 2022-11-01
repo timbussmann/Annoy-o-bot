d2a0d33cf048cb5f0c045f9a41339e83754b35a0
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-11-01 15:35:21 +0100

    first acceptance test draft

---
+++ Annoy-o-Bot.AcceptanceTests/FakeGithubInstallation.cs
@@ -0,0 +1,40 @@
+﻿using Annoy_o_Bot.GitHub;
+using Octokit;
+
+namespace Annoy_o_Bot.AcceptanceTests;
+
+class FakeGithubInstallation : IGitHubAppInstallation
+{
+    public bool Initialized { get; private set; }
+
+    public long InstallationId { get; private set; }
+
+    public Task Initialize(long installationId)
+    {
+        this.InstallationId = installationId;
+        this.Initialized = true;
+        return Task.CompletedTask;
+    }
+
+    private readonly Dictionary<string, string> files = new();
+    public void AddFileContent(string filePath, string content)
+    {
+        this.files.Add(filePath, content);
+    }
+
+    //TODO test behavior when file does not exist
+    public Task<string> ReadFileContent(string filePath, long repositoryId, string branchReference)
+    {
+        return Task.FromResult(this.files[filePath]);
+    }
+
+    public Task CreateCheckRun(NewCheckRun checkRun, long repositoryId)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task CreateComment(long repositoryId, string commitId, string comment)
+    {
+        return Task.CompletedTask;
+    }
+}
---
+++ Annoy-o-Bot.AcceptanceTests/When_adding_new_reminder.cs
@@ -0,0 +1,77 @@
+﻿using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging.Abstractions;
+using Newtonsoft.Json;
+using Xunit;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+namespace Annoy_o_Bot.AcceptanceTests;
+
+public class When_adding_new_reminder_to_default_branch
+{
+    private const string SignatureKey = "mysecretkey";
+
+    [Fact]
+    public async Task Should_store_reminder_in_database()
+    {
+        var headCommit = new CallbackModel.CommitModel
+        {
+            Added = new[]
+            {
+                ".reminders/test.json"
+            }
+        };
+        var callback = new CallbackModel
+        {
+            Installation = new CallbackModel.InstallationModel() { Id = Random.Shared.NextInt64() },
+            Repository = new CallbackModel.RepositoryModel() { Id = Random.Shared.NextInt64(),  DefaultBranch = "main"},
+            Ref = "refs/heads/main",
+            HeadCommit = headCommit,
+            Commits = new[] { headCommit }
+        };
+
+        var reminder = new Reminder()
+        {
+            Title = "Some title for the new reminder",
+            Date = DateTime.UtcNow.AddDays(10),
+            Interval = Interval.Weekly
+        };
+
+        HttpRequest request = new DefaultHttpRequest(new DefaultHttpContext());
+        var messageContent = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(callback, Formatting.None));
+        request.Body = new MemoryStream( messageContent);
+        request.Headers.Add("X-GitHub-Event", "push");
+        request.Headers.Add("X-Hub-Signature-256", "sha256=" + Convert.ToHexString(HMACSHA256.HashData(Encoding.UTF8.GetBytes(SignatureKey), messageContent)));
+
+        FakeReminderCollection documents = new FakeReminderCollection();
+
+        var appInstallation = new FakeGithubInstallation();
+        appInstallation.AddFileContent(callback.Commits[0].Added[0], JsonSerializer.Serialize(reminder));
+
+        var configurationBuilder = new ConfigurationBuilder();
+        configurationBuilder.AddInMemoryCollection(new Dictionary<string, string>
+        {
+           { "WebhookSecret", SignatureKey }
+        });
+
+        var handler = new CallbackHandler(appInstallation, configurationBuilder.Build());
+        var result = await handler.Run(request, documents, null!, NullLogger.Instance);
+
+        Assert.IsType<OkResult>(result);
+
+        var addedReminder = Assert.Single(documents.AddedDocuments);
+        Assert.Equal(callback.Installation.Id, addedReminder.InstallationId);
+        Assert.Equal(callback.Repository.Id, addedReminder.RepositoryId);
+        Assert.Equal(callback.Commits[0].Added[0], addedReminder.Path);
+        Assert.Equal(DateTime.MinValue, addedReminder.LastReminder);
+        Assert.Equal(reminder.Date, addedReminder.NextReminder);
+        Assert.Equivalent(reminder, addedReminder.Reminder);
+
+        //TODO verify comment
+    }
+}
