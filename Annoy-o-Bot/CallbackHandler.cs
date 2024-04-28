2990900c2bb5471cf98f64c189dfc95db0ae4c7b
Tim Bussmann <timbussmann@users.noreply.github.com>
2024-04-28 15:09:12 +0200

    small readability improvements

--- Annoy-o-Bot/CallbackHandler.cs
+++ Annoy-o-Bot/CallbackHandler.cs
@@ -13,6 +13,7 @@ using Annoy_o_Bot.GitHub;
 using Microsoft.Azure.Cosmos;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Primitives;
 
 namespace Annoy_o_Bot
 {
@@ -30,14 +31,9 @@ namespace Annoy_o_Bot
         {
             var cosmosWrapper = new CosmosClientWrapper(cosmosContainer);
             await GitHubHelper.ValidateRequest(req, configuration.GetValue<string>("WebhookSecret") ?? throw new Exception("Missing 'WebhookSecret' env var"), log);
-            if (!req.Headers.TryGetValue("X-GitHub-Event", out var callbackEvent) || callbackEvent != "push")
-            {
-                if (callbackEvent != "check_suite") // ignore check_suite events
-                {
-                    // this typically seem to be installation related events.
-                    log.LogWarning($"Non-push callback. 'X-GitHub-Event': '{callbackEvent}'");
-                }
 
+            if (!IsGitCommitCallback(req))
+            {
                 return new OkResult();
             }
 
@@ -57,7 +53,7 @@ namespace Annoy_o_Bot
             {
                 // if the last commit is a merge commit, ignore other commits as the merge commits contains all the relevant changes
                 // TODO: This behavior will be incorrect if a non-merge-commit contains this commit message. To be absolutely sure, we'd have to retrieve the full commit object and inspect the parent information. This information is not available on the callback object
-                commitsToConsider = new[] { commitsToConsider.Last() };
+                commitsToConsider = [commitsToConsider.Last()];
             }
 
             var fileChanges = CommitParser.GetChanges(commitsToConsider);
@@ -65,73 +61,102 @@ namespace Annoy_o_Bot
 
             if (requestObject.Ref.EndsWith($"/{requestObject.Repository.DefaultBranch}"))
             {
-                var newReminders = await LoadReminder(reminderChanges.New, requestObject, githubClient);
-                foreach (var (fileName, reminder) in newReminders)
-                {
-                    await CreateNewReminder(cosmosWrapper, requestObject, reminder, fileName, githubClient);
-                }
-
-                var updatedReminders = await LoadReminder(reminderChanges.Updated, requestObject, githubClient);
-                foreach (var (fileName, updatedReminder) in updatedReminders)
-                {
-                    var existingReminder = await cosmosWrapper.LoadReminder(fileName, requestObject.Installation.Id, requestObject.Repository.Id);
-                    if (existingReminder is null)
-                    {
-                        await CreateNewReminder(cosmosWrapper, requestObject, updatedReminder, fileName, githubClient);
-                    }
-                    else
-                    {
-                        existingReminder.Reminder = updatedReminder;
-                        // recalculate next reminder due time from scratch:
-                        existingReminder.NextReminder = new DateTime(updatedReminder.Date.Ticks, DateTimeKind.Utc);
-                        if (existingReminder.LastReminder >= existingReminder.NextReminder)
-                        {
-                            // reminder start date is in the past, re-calculate next reminder due date with interval based on new start date.
-                            existingReminder.CalculateNextReminder(DateTime.Now);
-                        }
-
-                        await cosmosWrapper.AddOrUpdateReminder(existingReminder);
-                        await githubClient.CreateComment(requestObject.HeadCommit.Id,
-                            $"Updated reminder '{updatedReminder.Title}' for {existingReminder.NextReminder:D}");
-                    }
-                }
-
-                await DeleteRemovedReminders(fileChanges.Deleted, cosmosWrapper, requestObject, githubClient);
+                await ApplyReminderDefinitions(reminderChanges, requestObject, githubClient, cosmosWrapper, fileChanges);
             }
             else
             {
-                List<(string, ReminderDefinition)> newReminders;
-                try
-                {
-                    newReminders = await LoadReminder(reminderChanges.New, requestObject, githubClient);
-                    newReminders.AddRange(await LoadReminder(reminderChanges.Updated, requestObject, githubClient));
-                }
-                catch (Exception e)
-                {
-                    await TryCreateCheckRun(githubClient, requestObject.Repository.Id,
-                        new NewCheckRun("annoy-o-bot", requestObject.HeadCommit.Id)
-                        {
-                            Status = CheckStatus.Completed,
-                            Conclusion = CheckConclusion.Failure,
-                            Output = new NewCheckRunOutput(
-                                "Invalid reminder definition",
-                                "The provided reminder seems to be invalid or incorrect." + e.Message)
-                        }, log);
-                    throw;
-                }
-
-                if (newReminders.Any())
-                {
-                    await TryCreateCheckRun(githubClient, requestObject.Repository.Id,
-                        new NewCheckRun("annoy-o-bot", requestObject.HeadCommit.Id)
-                        {
-                            Status = CheckStatus.Completed,
-                            Conclusion = CheckConclusion.Success
-                        }, log);
-                }
+                await ValidateReminderDefinitions(reminderChanges, requestObject, githubClient);
             }
 
             return new OkResult();
+        }
+
+        async Task ApplyReminderDefinitions(FileChanges reminderChanges, CallbackModel requestObject,
+            IGitHubRepository githubClient, CosmosClientWrapper cosmosWrapper, FileChanges fileChanges)
+        {
+            var newReminders = await LoadReminders(reminderChanges.New, requestObject, githubClient);
+            foreach (var (fileName, reminder) in newReminders)
+            {
+                await CreateNewReminder(cosmosWrapper, requestObject, reminder, fileName, githubClient);
+            }
+
+            var updatedReminders = await LoadReminders(reminderChanges.Updated, requestObject, githubClient);
+            foreach (var (fileName, updatedReminder) in updatedReminders)
+            {
+                var existingReminder = await cosmosWrapper.LoadReminder(fileName, requestObject.Installation.Id, requestObject.Repository.Id);
+                if (existingReminder is null)
+                {
+                    await CreateNewReminder(cosmosWrapper, requestObject, updatedReminder, fileName, githubClient);
+                }
+                else
+                {
+                    existingReminder.Reminder = updatedReminder;
+                    // recalculate next reminder due time from scratch:
+                    existingReminder.NextReminder = new DateTime(updatedReminder.Date.Ticks, DateTimeKind.Utc);
+                    if (existingReminder.LastReminder >= existingReminder.NextReminder)
+                    {
+                        // reminder start date is in the past, re-calculate next reminder due date with interval based on new start date.
+                        existingReminder.CalculateNextReminder(DateTime.Now);
+                    }
+
+                    await cosmosWrapper.AddOrUpdateReminder(existingReminder);
+                    await githubClient.CreateComment(requestObject.HeadCommit.Id,
+                        $"Updated reminder '{updatedReminder.Title}' for {existingReminder.NextReminder:D}");
+                }
+            }
+
+            await DeleteRemovedReminders(fileChanges.Deleted, cosmosWrapper, requestObject, githubClient);
+        }
+
+        async Task ValidateReminderDefinitions(FileChanges reminderChanges, CallbackModel requestObject,
+            IGitHubRepository githubClient)
+        {
+            List<(string, ReminderDefinition)> newReminders;
+            try
+            {
+                newReminders = await LoadReminders(reminderChanges.New, requestObject, githubClient);
+                newReminders.AddRange(await LoadReminders(reminderChanges.Updated, requestObject, githubClient));
+            }
+            catch (Exception e)
+            {
+                await TryCreateCheckRun(githubClient, requestObject.Repository.Id,
+                    new NewCheckRun("annoy-o-bot", requestObject.HeadCommit.Id)
+                    {
+                        Status = CheckStatus.Completed,
+                        Conclusion = CheckConclusion.Failure,
+                        Output = new NewCheckRunOutput(
+                            "Invalid reminder definition",
+                            "The provided reminder seems to be invalid or incorrect." + e.Message)
+                    }, log);
+                throw;
+            }
+
+            if (newReminders.Any())
+            {
+                await TryCreateCheckRun(githubClient, requestObject.Repository.Id,
+                    new NewCheckRun("annoy-o-bot", requestObject.HeadCommit.Id)
+                    {
+                        Status = CheckStatus.Completed,
+                        Conclusion = CheckConclusion.Success
+                    }, log);
+            }
+        }
+
+        bool IsGitCommitCallback(HttpRequest req)
+        {
+            if (!req.Headers.TryGetValue("X-GitHub-Event", out var callbackEvent) || callbackEvent != "push")
+            {
+                // Check for known callback types that we don't care
+                if (callbackEvent != "check_suite") // ignore check_suite events
+                {
+                    // record unknown callback types to further analyze them
+                    log.LogWarning($"Non-push callback. 'X-GitHub-Event': '{callbackEvent}'");
+                }
+
+                return false;
+            }
+
+            return true;
         }
 
         async Task CreateNewReminder(CosmosClientWrapper cosmosWrapper, CallbackModel requestObject, ReminderDefinition reminderDefinition, string fileName,
@@ -178,7 +203,7 @@ namespace Annoy_o_Bot
             }
         }
 
-        static async Task<List<(string, ReminderDefinition)>> LoadReminder(ICollection<string> filePaths, CallbackModel requestObject, IGitHubRepository installationClient)
+        static async Task<List<(string, ReminderDefinition)>> LoadReminders(ICollection<string> filePaths, CallbackModel requestObject, IGitHubRepository installationClient)
         {
             var results = new List<(string, ReminderDefinition)>(filePaths.Count); // potentially lower but never higher than number of files
             foreach (var filePath in filePaths)
