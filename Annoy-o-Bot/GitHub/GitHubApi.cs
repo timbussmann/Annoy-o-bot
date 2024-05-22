f968c47e27e2d42e118d8af8f05de79bff16647f
Tim Bussmann <timbussmann@users.noreply.github.com>
2024-05-22 22:15:52 +0200

    inline remaining github helper method

--- Annoy-o-Bot/GitHub/GitHubApi.cs
+++ Annoy-o-Bot/GitHub/GitHubApi.cs
@@ -65,5 +65,37 @@ public class GitHubApi : IGitHubApi
         }
     }
 
-    static Task<GitHubClient> GetInstallationClient(long installationId) => GitHubHelper.GetInstallationClient(installationId);
+    static async Task<GitHubClient> GetInstallationClient(long installationId)
+    {
+        // Use GitHubJwt library to create the GitHubApp Jwt Token using our private certificate PEM file
+        var appIntegrationId = Convert.ToInt32(Environment.GetEnvironmentVariable("GitHubAppId"));
+        var environmentVariablePrivateKeySource = new EnvironmentVariablePrivateKeySource("PrivateKey");
+        var generator = new GitHubJwtFactory(
+            environmentVariablePrivateKeySource,
+            new GitHubJwtFactoryOptions
+            {
+                AppIntegrationId = appIntegrationId,
+                ExpirationSeconds = 600 // 10 minutes is the maximum time allowed
+            }
+        );
+        var jwtToken = generator.CreateEncodedJwtToken();
+
+        var productHeaderValue = Environment.GetEnvironmentVariable("GitHubProductHeader");
+        // Use the JWT as a Bearer token
+        var appClient = new GitHubClient(new ProductHeaderValue(productHeaderValue))
+        {
+            Credentials = new Credentials(jwtToken, AuthenticationType.Bearer)
+        };
+        // Get the current authenticated GitHubApp
+        var app = await appClient.GitHubApps.GetCurrent();
+        // Create an Installation token for Insallation Id
+        var response = await appClient.GitHubApps.CreateInstallationToken(installationId);
+
+        // Create a new GitHubClient using the installation token as authentication
+        var installationClient = new GitHubClient(new ProductHeaderValue(productHeaderValue))
+        {
+            Credentials = new Credentials(response.Token)
+        };
+        return installationClient;
+    }
 }
